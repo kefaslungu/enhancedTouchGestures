4f84e00303a4b93bbbab0b3aea08d36a5ff9b493
Joseph Lee <joseph.lee22590@gmail.com>
2017-10-09 08:47:54 -0700

    Installer: prevent installation on Windows 8.
    Starting from add-on 17.10, Windows 8.1 or 10 is required due to support policy for Windows 8.

---
+++ addon/installTasks.py
@@ -0,0 +1,19 @@
+# enhancedTouchGestures/installTasks.py
+# Copyright 2017 Joseph Lee, released under GPL.
+
+# Provides needed routines during add-on installation and removal.
+# Mostly checks compatibility.
+# Routines are partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
+
+import sys
+import gui
+import wx
+import addonHandler
+addonHandler.initTranslation()
+
+def onInstall():
+	# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
+	if sys.getwindowsversion().build < 9600 and gui.messageBox(_("You are using an older version of Windows. This add-on requires Windows 8.1 or later. Are you sure you wish to install this add-on anyway?"),
+	# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
+	_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.NO:
+		raise RuntimeError("Old Windows version detected")
