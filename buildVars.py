ad09d450495188a340cda0c3b6998dd205cd4e33
Joseph Lee <joseph.lee22590@gmail.com>
2019-11-21 20:06:48 -0800

    NVDA compatibility: minimum NVDA version -> 2019.3

--- buildVars.py
+++ buildVars.py
@@ -28,7 +28,7 @@ addon_info = {
 	# File name for the add-on help file.
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported
-	"addon_minimumNVDAVersion" : "2018.2.0",
+	"addon_minimumNVDAVersion" : "2019.3.0",
 	# Last NVDA version supported/tested
 	"addon_lastTestedNVDAVersion" : "2019.3.0",
 	# Minimum Windows version supported
