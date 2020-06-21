9d0171ac7d76762f61cf0ead5f27f175215a9d20
Joseph Lee <joseph.lee22590@gmail.com>
2020-06-20 19:04:54 -0700

    NVDA compatibility: minimum = 2020.1, last tested = 2020.3

--- buildVars.py
+++ buildVars.py
@@ -28,9 +28,9 @@ addon_info = {
 	# File name for the add-on help file.
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported
-	"addon_minimumNVDAVersion": "2019.3.0",
+	"addon_minimumNVDAVersion": "2020.1.0",
 	# Last NVDA version supported/tested
-	"addon_lastTestedNVDAVersion": "2020.2.0",
+	"addon_lastTestedNVDAVersion": "2020.3.0",
 	# Minimum Windows version supported
 	"addon_minimumWindowsVersion": "6.3.9600",
 	# Add-on update channel (default is stable)
