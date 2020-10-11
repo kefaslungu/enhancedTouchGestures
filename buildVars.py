bee6cd8487fc5f8606e2f349d26505f933f91dc3
Joseph Lee <joseph.lee22590@gmail.com>
2020-10-11 13:41:48 -0700

    Add-on template: use latest teplate version, including lint fixes.

--- buildVars.py
+++ buildVars.py
@@ -3,15 +3,23 @@
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
-# Full getext (please don't change)
-_ = lambda x: x
+
+# Since some strings in `addon_info` are translatable,
+# we need to include them in the .po files.
+# Gettext recognizes only strings given as parameters to the `_` function.
+# To avoid initializing translations in this module we simply roll our own "fake" `_` function
+# which returns whatever is given to it as an argument.
+def _(arg):
+	return arg
+
 
 # Add-on information variables
 addon_info = {
-	# add-on Name, internal for nvda
+	# add-on Name/identifier, internal for NVDA
 	"addon_name": "enhancedTouchGestures",
 	# Add-on summary, usually the user visible name of the addon.
-	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
+	# Translators: Summary for this add-on
+	# to be shown on installation and add-on information.
 	"addon_summary": _("Enhanced Touch Gestures"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
@@ -23,23 +31,31 @@ addon_info = {
 	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support
 	"addon_url": "https://addons.nvda-project.org",
-	# File name for the add-on help file.
+	# Documentation file name
 	"addon_docFileName": "readme.html",
-	# Minimum NVDA version supported
+	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2020.1",
-	# Last NVDA version supported/tested
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
 	"addon_lastTestedNVDAVersion": "2020.4",
 	# Minimum Windows version supported
 	"addon_minimumWindowsVersion": "6.3.9600",
-	# Add-on update channel (default is stable)
+	# Add-on update channel (default is None, denoting stable releases,
+	# and for development releases, use "dev".)
+	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
 }
 
 
-import os.path
+import os.path  # NOQA
 
 # Define the python files that are the sources of your add-on.
-# You can use glob expressions here, they will be expanded.
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+# For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
+# the list can be written as follows:
+# pythonSources = ["addon/globalPlugins/*.py"]
+# For more information on SCons Glob expressions please take a look at:
+# https://scons.org/doc/production/HTML/scons-user/apd.html
 pythonSources = [os.path.join("addon", "globalPlugins", "enhancedTouchGestures.py")]
 
 # Files that contain strings for translation. Usually your python sources
