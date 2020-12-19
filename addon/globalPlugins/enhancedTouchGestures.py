5a0fdbdec05a26f6836abaf67d754972aed70d17
Joseph Lee <joseph.lee22590@gmail.com>
2020-12-19 12:49:34 -0800

    Update copyright header

--- addon/globalPlugins/enhancedTouchGestures.py
+++ addon/globalPlugins/enhancedTouchGestures.py
@@ -1,6 +1,6 @@
 # Enhanced touch gestures
 # A touchscreen global plugin for NVDA
-# Copyright 2013-2020 Joseph Lee and others, released under GPL.
+# Copyright 2013-2020 Joseph Lee, released under GPL.
 
 # Implements needed improvements for various touchscreen gestures.
 
