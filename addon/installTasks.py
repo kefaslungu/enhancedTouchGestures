fea51ed90d1c3e201b181d99913eaf9d5f925e94
Joseph Lee <joseph.lee22590@gmail.com>
2020-12-27 11:20:00 -0800

    Update copyright year

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,5 +1,5 @@
 # enhancedTouchGestures/installTasks.py
-# Copyright 2017-2020 Joseph Lee, released under GPL.
+# Copyright 2017-2021 Joseph Lee, released under GPL.
 
 # Provides needed routines during add-on installation and removal.
 # Mostly checks compatibility.
