b3b140a1e96144ebdc5581491f5d61e196c5737a
Joseph Lee <joseph.lee22590@gmail.com>
2021-09-23 22:22:52 -0700

    Manifest: remove minimum Windows version key.
    Preparation for a future add-on store: minimum Windows version is not really a manifest key to e used in a future add-ons store, so remove this. In build vars, keep it and add a note about it.

--- buildVars.py
+++ buildVars.py
@@ -38,6 +38,7 @@ addon_info = {
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
 	"addon_lastTestedNVDAVersion": "2021.2",
 	# Minimum Windows version supported
+	# Note: not to be included in the manifest, kept here to remind people about touch support requirement.
 	"addon_minimumWindowsVersion": "6.3.9600",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
