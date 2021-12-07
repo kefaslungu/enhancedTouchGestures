8780636f0499858d00ab8df0511d1429f4a5e34a
Joseph Lee <joseph.lee22590@gmail.com>
2021-12-07 10:20:33 -0800

    NVDA compatibility: last tested version -> 2021.3

--- buildVars.py
+++ buildVars.py
@@ -36,7 +36,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2021.2",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2021.2",
+	"addon_lastTestedNVDAVersion": "2021.3",
 	# Minimum Windows version supported
 	# Note: not to be included in the manifest, kept here to remind people about touch support requirement.
 	"addon_minimumWindowsVersion": "6.3.9600",
