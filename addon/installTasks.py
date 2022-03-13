74ee5da2f891f81ccc6be213c1e20eb558d4df66
Joseph Lee <joseph.lee22590@gmail.com>
2022-03-13 10:59:57 -0700

    Install tasks: present a message if running on Windows 8.1.
    As a future version of the add-on plans to end support for Windows 8.1, present a message if running on that version of Windows.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -14,14 +14,13 @@ def onInstall():
 	import winVersion
 	import gui
 	import wx
-	if winVersion.getWinVer() < winVersion.WIN81 and gui.messageBox(
-		_(
-			# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows
-			# (minSupportedVersion is the minimum version required for this add-on).
-			"You are using an older version of Windows. This add-on requires Windows 8.1 or later. "
-			"Are you sure you wish to install this add-on anyway?"
-		),
-		# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
-		_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION
-	) == wx.NO:
-		raise RuntimeError("Old Windows version detected")
+	if winVersion.getWinVer() < winVersion.WIN10:
+		gui.messageBox(
+			_(
+				# Translators: Dialog text shown when attempting to install the add-on on an older version of Windows.
+				"You are using an older version of Windows. "
+				"This is the last version of Enhanced Touch Gestures add-on to support this version of Windows."
+			),
+			# Translators: title of the dialog shown when trying to install the add-on on an old version of Windows.
+			_("Old Windows version"), wx.OK | wx.ICON_WARNING
+		)
