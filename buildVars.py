40bfca51892547b1ef3e433385103b395e3bb560
Joseph Lee <joseph.lee22590@gmail.com>
2026-08-11 19:30:55 -0600

    Changelog: add version 26.09 changelog on removal of add-on specific touch browse mode features

--- buildVars.py
+++ buildVars.py
@@ -26,9 +26,8 @@ addon_info = AddonInfo(
 	addon_version="26.08",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""* Removed add-on changelog from add-on help (readme) file.
-* The "default (all elements)" mode in touch browse mode is now called "default" mode to match NVDA 2026.2 messaging.
-* If running NVDA 2026.2, NVDA will no longer appear to do nothing or play error tones when performing browse touch mode commands in web browsers while navigator object is located somewhere other than the document content."""),
+	addon_changelog=_("""* NVDA 2026.2 or later is required.
+* Removed add-on specific version of touch browse mode in favor of NVDA's own touch browse mode functionality. Use NVDA's browse mode settings to configure touch browse mode elements."""),
 	# Author(s)
 	addon_author="Joseph Lee <joseph.lee22590@gmail.com>, Kefas Lungu <jameskefaslungu@gmail.com>",
 	# URL for the add-on documentation support
